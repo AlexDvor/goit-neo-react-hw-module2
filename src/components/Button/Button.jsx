05c180e602fadfcc20142caa60c953452a8e9784
AlexDvor <alexmatvichuk@gmail.com>
2025-12-06 20:25:10 +0100

    add s

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,9 @@
+import s from './Button.module.css';
+
 const Button = ({ children, onHandleClick, isVisible = true }) => {
 	return (
 		<button
+			className={s.btn}
 			onClick={() => onHandleClick(children)}
 			style={{ display: isVisible ? 'inline-block' : 'none' }}
 		>
